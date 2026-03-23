8228c6445529209aeffa9ed50da653512fd915eb
Laura <lauracarrasco77@gmail.com>
2026-03-23 15:44:28 -0600

    primer cambio

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import { browserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Encabezado from "./components/navegacion/Encabezado";
+
+import Inicio from "./views/Inicio";
+import Categorias from "./views/Categorias";
+import Catalogo from "./views/Catalogo";
+import Productos from "./views/Productos";
+import Login from "./views/Login";
+import RutaProtegida from "./components/RutaProtegida";
+import Pagina404 from "./views/Pagina404";
+
+import "./App.css";
